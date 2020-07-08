b20da932ac07c6ea13bee9f3947510053d6d9ee8
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-07-08 15:08:22 +0600

    Get data from db

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const mysql = require('mysql');
 const faker = require('faker');
+
+// DB connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'join_us'
+});
+
+const queryString = "SELECT * FROM users";
+connection.query(queryString, (error, result, fields) => {
+  if(error) throw error;
+  console.log(`email: ${result[0].email}`)
+});
+connection.end();
